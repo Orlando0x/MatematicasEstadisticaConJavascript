79d8aab832bcb4174720bffaa2c370c004b2f790
Orlando0x <rodor29@gmail.com>
2022-08-11 16:55:02 -0400

    Reto sobre promedio - Optimizado

--- calcularPromedio.js
+++ calcularPromedio.js
@@ -5,10 +5,8 @@ function promedio(){
     let suma = 0;
     for(let element of array){
         suma = suma + element;
-            if(element === array[array.length-1]){
-                let promedio = suma / array.length;
-                console.log(promedio.toFixed(2))
-            }
-    }
+        }
+    let promedio = suma / array.length;
+    return promedio.toFixed(2);
 }
 promedio(array);

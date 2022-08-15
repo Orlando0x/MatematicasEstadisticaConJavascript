c09e8814c978a745e2c3eb6ab3d4638db0c1d202
Orlando0x <rodor29@gmail.com>
2022-08-15 16:18:33 -0400

    Proyecto y curso terminado

--- reestructuracion-Informacion.js
+++ reestructuracion-Informacion.js
@@ -71,4 +71,81 @@ function medianaEmpresa (empresa, year){
     return mediana;
 
 }
-medianaEmpresa("MarketerosCOL",2022);
+
+
+function proyeccionPorEmpresa(empresa) {
+    if(!empresas[empresa]){
+        console.warn("Esta empresa no existe")
+    } else {
+        const  empresaYears = Object.keys(empresas[empresa]);
+        console.log(empresaYears)
+        const listaMedianaYears = empresaYears.map((year) => {
+            return medianaEmpresa(empresa, year)
+        })
+        console.log(listaMedianaYears);
+        
+    }
+
+}
+proyeccionPorEmpresa("Freelance");
+
+
+// Calcular la mediana del salario de cada persona
+function medianaPersona (person){
+
+    const juanitaSalariosLista = (findPerson(person).trabajos).map((element) =>
+        element.salario
+    );
+
+    const medianaSalarios = MathCalculos.calcularMediana(juanitaSalariosLista);
+    
+
+    return medianaSalarios;
+    }
+
+medianaPersona("Juanita");
+
+
+// Lista de personas
+const listaPersonas = salarios.map((person) => {
+    return person.name;
+} )
+
+const medianaLista = listaPersonas.map((person) => {
+    return medianaPersona(person);
+} )
+
+//Ordenar ista de medianas de cada persona, de menor a mayor
+const medianaListaOrdenada = medianaLista.sort((a , b) => {
+    if(a > b) return 1;
+    if(a < b) return -1;
+    return 0;
+    });
+    console.log(medianaListaOrdenada)
+
+//Calcular la mediana de salarios entre TODAS las personas
+function medianaGeneral() {
+    const medianaGeneral = MathCalculos.calcularMediana(medianaLista);
+    console.log(medianaGeneral);
+}
+medianaGeneral();
+
+//Top 10% con mejor salario
+
+function medianaTop10 (listaSalarios) {
+const topSalarios = [];
+const cantidadPersonas = listaSalarios.length * 0.1;
+console.log(cantidadPersonas)
+
+for(let i = 1; i <= cantidadPersonas; i++){
+    topSalarios.push(listaSalarios[listaSalarios.length - i])
+}
+console.log(topSalarios);
+
+//Mediana del top 10% de salarios
+const medianaTop10 = MathCalculos.calcularMediana(topSalarios);
+console.log(medianaTop10);
+return medianaTop10;
+}
+
+medianaTop10(medianaListaOrdenada);
